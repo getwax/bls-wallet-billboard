ab1e981b75a615f944999e7cdf9c557a9307f79f
Andrew Morris <voltrevo@gmail.com>
2022-03-24 00:34:37 +0000

    Set up billboards

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,4 +1,4 @@
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 import * as React from "react";
 import { Billboard__factory } from "../typechain";
 import loadConfig from "./loadConfig";
@@ -6,30 +6,56 @@ import loadConfig from "./loadConfig";
 const provider = new ethers.providers.Web3Provider((window as any).ethereum);
 
 const App: React.FunctionComponent = () => {
-  const [blockNo, setBlockNo] = React.useState<number>();
-  const [billboardHash, setBillboardHash] = React.useState<string>();
+  const [billboardInfo, setBillboardInfo] = React.useState<{
+    billboardHash: string;
+    leaseExpiry: BigNumber;
+    dailyRent: BigNumber;
+  }>();
 
   React.useEffect(() => {
     (async () => {
-      setBlockNo(await provider.getBlockNumber());
-
       const billboard = Billboard__factory.connect(
         loadConfig().billboardAddress,
         provider
       );
 
-      console.log(billboard.address);
-
-      setBillboardHash(await billboard.billboardHash());
+      setBillboardInfo({
+        billboardHash: await billboard.billboardHash(),
+        leaseExpiry: await billboard.leaseExpiry(),
+        dailyRent: await billboard.dailyRent(),
+      });
     })().catch(console.error);
   }, []);
+
+  if (billboardInfo === undefined) {
+    return <>Loading...</>;
+  }
+
+  const leaseExpiryDate = new Date(1000 * billboardInfo.leaseExpiry.toNumber());
+  const leaseActive = Date.now() > leaseExpiryDate.getTime();
+
+  const effectiveBillboardHash = leaseActive
+    ? billboardInfo.billboardHash
+    : ethers.constants.HashZero;
 
   return (
     <>
       <h1>Billboard Sample dApp</h1>
 
-      <p>Block no {blockNo}</p>
-      <p>Billboard hash {billboardHash}</p>
+      {billboardInfo && (
+        <>
+          <div
+            style={{
+              border: "3px solid black",
+              width: "80vw",
+              height: "25vw",
+              background: `url(/billboards/${effectiveBillboardHash}.png) center no-repeat`,
+            }}
+          />
+
+          <div>{leaseExpiryDate.toString()}</div>
+        </>
+      )}
     </>
   );
 };
